c7c45988563c38ad80adeb745c7df614b58ad753
Code <143876777+JavaIsHot@users.noreply.github.com>
2024-09-23 00:53:40 -0700

    Update PlayerData.java

--- PlayerData.java
+++ PlayerData.java
@@ -4,45 +4,45 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.persistence.PersistentDataType;
 
 public class PlayerData {
-    public static PersistentDataContainer getContainer(Player player) {
-        return player.getPersistentDataContainer();
-    }
+	public static PersistentDataContainer getContainer(Player player) {
+		return player.getPersistentDataContainer();
+	}
 
-    public static NamespacedKey key(Enum<?> key) {
-        return new NamespacedKey(new MyPluginMainClass(), key.toString().toLowercase());
-    }
+	public static NamespacedKey key(Enum<?> key) {
+		return new NamespacedKey(new UntitledBallGame(), key.toString().toLowercase());
+	}
 
-    @SuppressWarnings("all")
-    public static String getString(Player player, String data) {
-        return getContainer(player).get(key(data), PersistentDataType.STRING);
-    }
+	@SuppressWarnings("all")
+	public static String getString(Player player, Enum<?> key) {
+		return getContainer(player).get(key(key), PersistentDataType.STRING);
+	}
 
-    @SuppressWarnings("all")
-    public static void setString(Player player, Enum<?> key, String data) {
-        getContainer(player).set(key(key), PersistentDataType.STRING, data);
-    }
-    
-    @SuppressWarnings("all")
-    public static double getDouble(Player player, String data) {
-        return getContainer(player).get(key(data), PersistentDataType.DOUBLE);
-    }
-    
-    @SuppressWarnings("all")
-    public static void setDouble(Player player, Enum<?> key, Double data) {
-        getContainer(player).set(key(key), PersistentDataType.DOUBLE, data);
-    }
-    
-    @SuppressWarnings("all")
-    public static boolean getBoolean(Player player, String data) {
-        return getContainer(player).get(key(data), PersistentDataType.DOUBLE);
-    }
-    
-    @SuppressWarnings("all")
-    public static void setBoolean(Player player, Enum<?> key, Double data) {
-        getContainer(player).set(key(key), PersistentDataType.DOUBLE, data);
-    }
+	@SuppressWarnings("all")
+	public static void setString(Player player, Enum<?> key, String data) {
+		getContainer(player).set(key(key), PersistentDataType.STRING, data);
+	}
 
-    public static void initiate(Player player) {
-        PlayerData.setDouble(player, ExampleEnum.CASH, 999);
-    }
+	@SuppressWarnings("all")
+	public static double getDouble(Player player, Enum<?> key) {
+		return getContainer(player).get(key(key), PersistentDataType.DOUBLE);
+	}
+
+	@SuppressWarnings("all")
+	public static void setDouble(Player player, Enum<?> key, Double data) {
+		getContainer(player).set(key(key), PersistentDataType.DOUBLE, data);
+	}
+
+	@SuppressWarnings("all")
+	public static boolean getBoolean(Player player, Enum<?> key) {
+		return getContainer(player).get(key(key), PersistentDataType.BOOLEAN);
+	}
+
+	@SuppressWarnings("all")
+	public static void setBoolean(Player player, Enum<?> key, boolean data) {
+		getContainer(player).set(key(key), PersistentDataType.BOOLEAN, data);
+	}
+
+	public static void initiate(Player player) {
+		PlayerData.setDouble(player, ExampleEnumClass.CASH, 999);
+	}
 }
