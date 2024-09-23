06a1458c1afadbd81281643366bc0cb65b4c5087
Code <143876777+JavaIsHot@users.noreply.github.com>
2024-09-23 00:54:35 -0700

    Update PlayerData.java

--- PlayerData.java
+++ PlayerData.java
@@ -9,7 +9,7 @@ public class PlayerData {
 	}
 
 	public static NamespacedKey key(Enum<?> key) {
-		return new NamespacedKey(new YourPluginMainClass(), key.toString().toLowercase());
+		return new NamespacedKey(new YourPluginMainClass(), key.toString().toLowerCase());
 	}
 
 	@SuppressWarnings("all")
